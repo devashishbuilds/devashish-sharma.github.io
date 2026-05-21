bdf2d5aeacbb37be4d9f7eae24bd5bf5e1537ab2
Devashish Sharma <devashish.unibo@gmail.com>
2026-05-21 22:53:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -348,7 +348,7 @@ const portfolioData = {
     subtitle: "Programmable RGB LED Matrix",
     summary: "A smart backpack featuring a programmable pixel display built using a custom matrix of RGB LEDs.",
     bullets: ["Custom LED hardware assembly.", "Custom graphics mapping."],
-    presentation: [ "images/backpack-img-1.jpg" ]
+    presentation: [ "images/project_smart_bag/bag1.png" ]
   },
   
   // ================= NEW PROJECTS ADDED =================
